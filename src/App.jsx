149e4ac16ab151ae3e00ba221bbfbc7be32758c5
LuzDD <60977540+LuzDD@users.noreply.github.com>
2025-04-04 18:53:02 -0500

    Cambio Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
